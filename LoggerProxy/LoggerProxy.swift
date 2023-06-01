8b7b9b4718bb5a5c919e22b5d83686707dc73b2e
chenrenwei <chenrenwei@yy.com>
2023-06-01 17:55:15 +0800

    增加日志模块控制

--- LoggerProxy/LoggerProxy.swift
+++ LoggerProxy/LoggerProxy.swift
@@ -40,7 +40,7 @@ public class LoggerProxy {
     }
 
     /// 日志输出函数
-    public typealias LogFunc = (Level, () -> Date /* time */, StaticString /* file */, StaticString /* func */, Int /* line */, String /* label */, () -> String /* msg */ ) -> Void
+    public typealias LogFunc = (Level, () -> Date /* time */, String /* module name */, StaticString /* file */, StaticString /* func */, Int /* line */, String /* label */, () -> String /* msg */ ) -> Void
 
     /// 共享日志对象
     public static let shared = LoggerProxy()
@@ -49,14 +49,14 @@ public class LoggerProxy {
     private static let iso8601DateFormatter = ISO8601DateFormatter()
 
     /// 默认控制台日志输出
-    public static let defaultConsoleLogger: LogFunc = { level, now, file, funcName, line, tag, msg in
+    public static let defaultConsoleLogger: LogFunc = { level, now, module, file, funcName, line, tag, msg in
         let time = iso8601DateFormatter.string(from: now())
         let msg2 = msg().replacingOccurrences(of: "\n", with: "~n")
-        print("\(time) [\(level.desc())] \(funcName):\(line) [\(tag)] \(msg2) ### \(file)")
+        print("\(time) [\(level.desc())] \(module):\(funcName):\(line) [\(tag)] \(msg2) ### \(file)")
     }
 
     /// 空白日志输出
-    public static let defaultEmptyLogger: LogFunc = { _, _, _, _, _, _, _ in
+    public static let defaultEmptyLogger: LogFunc = { _, _, _, _, _, _, _, _ in
     }
 
     /// 实际代理的日志输出
@@ -77,7 +77,12 @@ public class LoggerProxy {
     public var enableAsync: Bool = false
 
     /// 日志等级详细控制
+    /// 针对 logTag 控制
     public var logTagControl: Dictionary<LogTag, Level> = Dictionary()
+    /// 针对 module 控制
+    public var moduleLogControl: Dictionary<String, Level> = Dictionary()
+    /// 针对 module中 LogTag控制
+    public var moduleLogTagControl: Dictionary<String, Dictionary<LogTag, Level>> = Dictionary()
 
     /// 初始化日志代理
     public init() {
@@ -94,38 +99,71 @@ public class LoggerProxy {
 
     /// 打印日志
     public func appendLog(level: Level, tag: String, msg: @escaping AsyncLogMessage, file: StaticString, funcName: StaticString, line: Int) {
-        if level.rawValue >= (logTagControl[tag] ?? acceptLevel).rawValue {
-            let now: () -> Date = { Date() }
-            let logFunc = logFunction
-            if enableAsync {
-                logQueue.async {
-                    logFunc(level, now, file, funcName, line, tag, msg)
-                }
+        var specilControl = false
+        // tag 控制
+        if let tagLevel = logTagControl[tag] {
+            if tagLevel.rawValue > level.rawValue {
+                return
             } else {
-                logFunc(level, now, file, funcName, line, tag, msg)
+                specilControl = true
             }
+        }
+
+        let module = file.withUTF8Buffer({ String(decoding: $0, as: UTF8.self).components(separatedBy: "/").first ?? "" })
+        /// 模块控制
+        if let moduleLevel = moduleLogControl[module] {
+            if moduleLevel.rawValue > level.rawValue {
+                return
+            } else {
+                specilControl = true
+            }
+        }
+
+        /// 模块Tag控制
+        if let moduleControl = moduleLogTagControl[module],
+           let tagLevel = moduleControl[tag] {
+            if tagLevel.rawValue > level.rawValue {
+                return
+            } else {
+                specilControl = true
+            }
+        }
+        // 通用控制
+        if !specilControl,
+           acceptLevel.rawValue > level.rawValue {
+            return
+        }
+
+        let now: () -> Date = { Date() }
+        let logFunc = logFunction
+        if enableAsync {
+            logQueue.async {
+                logFunc(level, now, module, file, funcName, line, tag, msg)
+            }
+        } else {
+            logFunc(level, now, module, file, funcName, line, tag, msg)
         }
     }
 }
 
 extension LoggerProxy {
-    public static func VLog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #file, funcName: StaticString = #function, line: Int = #line) {
+    public static func VLog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #fileID, funcName: StaticString = #function, line: Int = #line) {
         LoggerProxy.shared.appendLog(level: Level.Verbose, tag: tag, msg: msg, file: file, funcName: funcName, line: line)
     }
 
-    public static func DLog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #file, funcName: StaticString = #function, line: Int = #line) {
+    public static func DLog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #fileID, funcName: StaticString = #function, line: Int = #line) {
         LoggerProxy.shared.appendLog(level: Level.Debug, tag: tag, msg: msg, file: file, funcName: funcName, line: line)
     }
 
-    public static func ILog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #file, funcName: StaticString = #function, line: Int = #line) {
+    public static func ILog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #fileID, funcName: StaticString = #function, line: Int = #line) {
         LoggerProxy.shared.appendLog(level: Level.Info, tag: tag, msg: msg, file: file, funcName: funcName, line: line)
     }
 
-    public static func WLog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #file, funcName: StaticString = #function, line: Int = #line) {
+    public static func WLog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #fileID, funcName: StaticString = #function, line: Int = #line) {
         LoggerProxy.shared.appendLog(level: Level.Warn, tag: tag, msg: msg, file: file, funcName: funcName, line: line)
     }
 
-    public static func ELog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #file, funcName: StaticString = #function, line: Int = #line) {
+    public static func ELog(tag: String, msg: @autoclosure @escaping AsyncLogMessage, file: StaticString = #fileID, funcName: StaticString = #function, line: Int = #line) {
         LoggerProxy.shared.appendLog(level: Level.Error, tag: tag, msg: msg, file: file, funcName: funcName, line: line)
     }
 }
